5fe4f66a1c31f5000297949a5f8aabbce6c180e5
MasterMario4 <mastermario4@gmail.com>
2019-09-30 23:30:27 +0200

    Standard Messages release 1.0
    A simple class used to store all the methods responsible for showing various messages in the console. So far, preliminary welcome and goodbye messages have been added.

---
+++ src/main/java/StandardMessages.java
@@ -0,0 +1,9 @@
+public class StandardMessages {
+    public static void WelcomeMessage(){
+        System.out.println("Welcome to Backup Commander!");
+    }
+
+    public static void GoodbyeMessage(){
+        System.out.println("Have a nice day!");
+    }
+}
