6e413efb481dc3d552e601b71a6274e73d052658
Łukasz Matuszewski <lumat@op.pl>
2019-10-01 23:52:59 +0200

    creating logs
    creating log file and writing logs into it

--- src/main/java/com/log/LogPrinter.java
+++ src/main/java/com/log/LogPrinter.java
@@ -1,4 +1,44 @@
 package com.log;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+
 public class LogPrinter {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void printLog() {
+        System.out.println("What kind of log would you like to print? [F] - full, [S] - simple");
+        switch (scanner.nextLine()) {
+            case "F":
+                printFullLog();
+                break;
+            case "S":
+                printSimpleLog();
+                break;
+            default:
+                System.out.println("Wrong key");
+        }
+    }
+
+    private static void printFullLog() {
+
+    }
+
+    private static void printSimpleLog() {
+
+    }
+
+    private static List<String> readLogFile() {
+        return null;
+    }
+
+    private static boolean logFileExist(Path repoPath) {
+        return Files.exists(repoPath);
+    }
+
+    private static boolean repoExist(Path repoPath) {
+        return Files.exists(repoPath);
+    }
 }
