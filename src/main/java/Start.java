9e187585392f924c59c767cda624347fd0f59fe8
Grzegorz <serwacy2@gmail.com>
2019-10-02 23:15:36 +0200

    testing classes in main (WIP)

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,7 +1,28 @@
+import com.saver.FileManager;
+import com.saver.FileSaver;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Start {
 
     public static void main(String[] args) {
         System.out.println("Starting app...");
-        MainMenu.start();
+
+//        File sourceFile = new File("file.txt");
+//        File destFile = new File("dest\\file.txt");
+//        try {
+//            FileSaver.copyFile(sourceFile, destFile);
+//        }
+//        catch(IOException e){
+//            System.out.println(e);
+//        }
+        File repoPath = new File("C:\\Users\\User\\IdeaProjects\\BackupCommander\\.repo");
+        try{
+            FileManager.copyDir(repoPath);
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
     }
 }
