a207c1e6949ae1751964dd498e74f9d1c130946d
Grzegorz <serwacy2@gmail.com>
2019-10-06 22:41:53 +0200

    changed Start class for examples

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,5 +1,4 @@
 import com.saver.FileManager;
-import com.saver.FileSaver;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,17 +8,10 @@ public class Start {
     public static void main(String[] args) {
         System.out.println("Starting app...");
 
-//        File sourceFile = new File("file.txt");
-//        File destFile = new File("dest\\file.txt");
-//        try {
-//            FileSaver.copyFile(sourceFile, destFile);
-//        }
-//        catch(IOException e){
-//            System.out.println(e);
-//        }
-        File repoPath = new File("C:\\Users\\User\\IdeaProjects\\BackupCommander\\.repo");
+        File srcFile = new File("C:\\Users\\User\\IdeaProjects\\BackupCommander");
+        File dstFile = new File("C:\\Users\\User\\IdeaProjects\\BackupCommander\\.repo");
         try{
-            FileManager.copyDir(repoPath);
+            FileManager.copyDir(srcFile,dstFile);
         }
         catch (IOException e){
             System.out.println(e);
