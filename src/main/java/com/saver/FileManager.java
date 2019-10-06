05144ed0495732baf07212db5dd994d68a74a42c
Grzegorz <serwacy2@gmail.com>
2019-10-06 21:55:33 +0200

    updated to copy all files to dest folder (except .repo)

--- src/main/java/com/saver/FileManager.java
+++ src/main/java/com/saver/FileManager.java
@@ -1,13 +1,31 @@
 package com.saver;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.FalseFileFilter;
+import org.apache.commons.io.filefilter.FileFileFilter;
+
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
-import java.nio.file.Path;
+import java.util.Set;
 
 public class FileManager {
-    public static void copyDir(File repoPath) throws IOException {
-        if(!repoPath.exists()){
-            new File(String.valueOf(repoPath)).mkdirs();
-        }
+    public static void copyFile(File sourceFile, File destFile) throws IOException {
+        FileUtils.copyFile(sourceFile, destFile);
+    }
+
+    public static void copyDir(File srcFile, File dstFile) throws IOException {
+        FileFilter repoFilter = new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                if(pathname.equals(dstFile)){
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        };
+        FileUtils.copyDirectory(srcFile,dstFile,repoFilter);
     }
 }
