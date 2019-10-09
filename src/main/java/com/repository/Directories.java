a7ac7ccb1787fc32d22f5fe9845375f6271e53e0
Grzegorz <serwacy2@gmail.com>
2019-10-09 22:55:25 +0200

    further changes to repo classes

---
+++ src/main/java/com/repository/Directories.java
@@ -0,0 +1,73 @@
+package com.repository;
+
+import java.util.Scanner;
+
+public class Directories {
+    public static String workingDir;
+    public static String repoDir;
+
+    public static String getWorkingDir() {
+        return workingDir;
+    }
+
+    public static void setWorkingDir(String workingDir) {
+        Directories.workingDir = workingDir;
+    }
+
+    public static String getRepoDir() {
+        return repoDir;
+    }
+
+    public static void setRepoDir(String repoDir) {
+        Directories.repoDir = repoDir;
+    }
+
+    public static void directoryInit(){
+        String workingDirectory;
+
+        System.out.println("Please state how would you like to create a directory:");
+        System.out.println("[1] - Name a full path.");
+        System.out.println("[2] - Name Directory elements.");
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt(); //będzie trzeba zrobić idiotoodporne
+
+        switch (choice) {
+            case 1:
+                RepoMessages.fullPathInstructions();
+                do {
+                    try {
+                        Scanner scanner1 = new Scanner(System.in);
+                        workingDirectory = scanner1.nextLine();
+                        workingDirectory += "/.repo";
+                        Directories.setRepoDir(workingDirectory);
+                        break;
+                    } catch (Exception e) {
+                        System.out.println("Invalid path.");
+                        RepoMessages.fullPathInstructions();
+                    }
+                } while (true);
+                break;
+            case 2:
+                System.out.println("Please enter directory elements. Press enter after each element to confirm. Type [-] to finish.");
+                workingDirectory = "";
+                do {
+                    Scanner scanner1 = new Scanner(System.in);
+                    String temp = scanner1.nextLine();
+                    if (temp.equals("-")) {
+                        break;
+                    } else {
+                        //Maybe change this for a more optimal method like StringBuffer.append()?
+                        workingDirectory += temp + "/";
+                    }
+                } while (true);
+
+                workingDirectory += ".repo";
+                Directories.setRepoDir(workingDirectory);
+                break;
+            default:
+                //to be improved in a future release
+                System.out.println("wrong choice");
+        }
+    }
+}
--- src/main/java/com/repository/RepoManager.java
+++ src/main/java/com/repository/RepoManager.java
@@ -4,103 +4,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 
 public class RepoManager {
     public static Repo repo;
     //    Maybe inlcude list of repos? To be discussed.
     //    List<Repo> repoList = new ArrayList<>();
-    public static void startRepoManager() {
-        int choice;
-        RepoMessages.startingInstructions();
-        do {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                choice = scanner.nextInt();
-                break;
-            } catch (Exception e) {
-                System.out.println("Invalid choice! Please try again.");
-                RepoMessages.startingInstructions();
-            }
-        } while (true);
 
-        switch (choice) {
-            case 1:
-                repoInit();
-                break;
-            case 2:
-                System.out.println("This will redirect to main menu in future realeases");
-//                mainMenu.start();
-                break;
-            default:
-                //loop here or sth
-                break;
-        }
-    }
-
-
-    private static void repoInit() {
-        String workingDirectory;
+    public static void repoInit() {
         Path directoryPath;
 
-        System.out.println("Please state how would you like to create a directory:");
-        System.out.println("[1] - Name a full path.");
-        System.out.println("[2] - Name Directory elements.");
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                RepoMessages.fullPathInstructions();
-                do {
-                    try {
-                        Scanner scanner1 = new Scanner(System.in);
-                        workingDirectory = scanner1.nextLine();
-
-                        workingDirectory += "/.repo";
-                        directoryPath = Paths.get(workingDirectory);
-                        break;
-                    } catch (Exception e) {
-                        System.out.println("Invalid path.");
-                        RepoMessages.fullPathInstructions();
-                    }
-                } while (true);
-                repo = new Repo(directoryPath);
-                break;
-
-            case 2:
-                System.out.println("Please enter directory elements. Press enter after each element to confirm. Type [-] to finish.");
-                workingDirectory = "";
-                do {
-                    Scanner scanner1 = new Scanner(System.in);
-                    String temp = scanner1.nextLine();
-                    if (temp.equals("-")) {
-                        break;
-                    } else {
-                        //Maybe change this for a more optimal method like StringBuffer.append()?
-                        workingDirectory += temp;
-                        workingDirectory += "/";
-                    }
-                } while (true);
-                workingDirectory += ".repo";
-                directoryPath = Paths.get(workingDirectory);
-                repo = new Repo(directoryPath);
-                break;
-            default:
-                //to be improved in a future release
-                System.out.println("wrong choice");
-        }
         try {
+            directoryPath = Paths.get(Directories.getRepoDir());
+            repo = new Repo(directoryPath);
             existsOrCreate(repo.getDirectoryPath());
         } catch (IOException e) {
             e.printStackTrace(); //To be improved in future release.
         }
     }
-
-//    private static String setRepoName() {
-//        System.out.println("Please enter repository name:");
-//        Scanner scanner = new Scanner(System.in);
-//        return scanner.nextLine();
-//    }
 
     private static void existsOrCreate(Path path) throws IOException {
         //Throws exception if a path parent directory does not exist. To be improved in future release.
